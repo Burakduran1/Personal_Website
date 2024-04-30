585d132ec4df199e4c1682b32697cfb69aaf0971
Burak Can Duran <burakduran12491@gmail.com>
2024-04-30 09:56:08 +0300

    First Commit

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db= new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.Abouts.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Experience()
+        {
+            var experience = db.Experiences.ToList();
+            return PartialView(experience);
+        }
+
+        public PartialViewResult Education()
+        {
+            var education = db.Educations.ToList();
+            return PartialView(education);
+        }
+
+        public PartialViewResult Skill()
+        {
+            var skill = db.SKILLS.ToList();
+            return PartialView(skill);
+        }
+
+        public PartialViewResult Interest()
+        {
+            var interest = db.Interests.ToList();
+            return PartialView(interest);
+        }
+
+        public PartialViewResult Award()
+        {
+            var avards = db.Awards.ToList();
+            return PartialView(avards);
+        }
+
+        [HttpGet] /*Page loading*/
+        public PartialViewResult Contact()
+        {
+            return PartialView();
+        }
+        [HttpPost] /*Page Action*/
+        public PartialViewResult Contact(Communicaiton c)
+        {
+            c.Date = DateTime.Parse(DateTime.Now.ToString());
+            db.Communicaitons.Add(c);
+            db.SaveChanges();
+            return PartialView();
+        }
+
+    }
+}
---
+++ MvcCv/Controllers/ExperienceController.cs
@@ -0,0 +1,59 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        // GET: Experience
+        ExperinceRepository repo = new ExperinceRepository();
+        public ActionResult Index()
+        {
+            var value = repo.List();
+            return View(value);
+        }
+
+        [HttpGet]
+        public ActionResult AddExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddExperience(Experience p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteExperience(int id)
+        {
+            Experience t = repo.Find(x => x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public ActionResult EditExperience(int id)
+        {
+            Experience t = repo.Find(x => x.ID == id);
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult EditExperience(Experience p)
+        {
+            Experience t = repo.Find(x => x.ID == p.ID);
+            t.Title = p.Title;
+            t.SubTitle = p.SubTitle;
+            t.Date = p.Date;
+            t.Description = p.Description;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
